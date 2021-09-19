3064a38e14d58eef954e89885fae80399dc48e62
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2021-09-19 15:13:53 +0200

    Update initial_config.sh

--- initial_config.sh
+++ initial_config.sh
@@ -103,26 +103,6 @@ echo
 echo
 echo
 
-echo "Adding new user"
-echo "==============="
-read -p "Press ENTER to start..."
-echo
-echo 'Installing "sudo"...'
-pacman -S sudo --noconfirm
-echo 'Allowing members of group "wheel" to use "sudo"...'
-sed -i 's_# %wheel ALL=(ALL) ALL_%wheel ALL=(ALL) ALL_' /etc/sudoers
-echo
-echo 'Creating new user that is a member of group "wheel"...'
-read -p 'Username: ' username_var
-read -s -p 'Password: ' password_var
-#useradd -m -G wheel -s /bin/bash $username_var
-useradd -m -g users -G wheel -s /bin/bash $username_var
-echo "$username_var:$password_var" | chpasswd
-echo
-echo
-echo
-echo
-
 echo "======================="
 echo "= Rebooting system... ="
 echo "======================="
