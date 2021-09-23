042ad729a5c73975ce516e5d0f2508e654513093
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2021-09-23 17:29:42 +0200

    Update install_vaultwarden.sh

--- install_vaultwarden.sh
+++ install_vaultwarden.sh
@@ -27,14 +27,14 @@ read -p "Press ENTER to start..."
 echo
 
 echo "Enabling Web Vault..."
-sed 's@^# IP_HEADER=.*@IP_HEADER=X-Forwarded-For@' /etc/vaultwarden.env
-sed 's@^# WEB_VAULT_FOLDER=.*@WEB_VAULT_FOLDER=/usr/share/webapps/vaultwarden-web@' /etc/vaultwarden.env
-sed 's@^# WEB_VAULT_ENABLED=.*@WEB_VAULT_ENABLED=true@' /etc/vaultwarden.env
+sed -i 's@^# IP_HEADER=.*@IP_HEADER=X-Forwarded-For@' /etc/vaultwarden.env
+sed -i 's@^# WEB_VAULT_FOLDER=.*@WEB_VAULT_FOLDER=/usr/share/webapps/vaultwarden-web@' /etc/vaultwarden.env
+sed -i 's@^# WEB_VAULT_ENABLED=.*@WEB_VAULT_ENABLED=true@' /etc/vaultwarden.env
 echo
 
 echo "Enabling Admin Interface..."
 admin_token_var=`openssl rand -base64 48`
-sed "s@^# ADMIN_TOKEN=.*@ADMIN_TOKEN=$admin_token_var@" /etc/vaultwarden.env
+sed -i "s@^# ADMIN_TOKEN=.*@ADMIN_TOKEN=$admin_token_var@" /etc/vaultwarden.env
 echo "You Admin Token is: $admin_token_var"
 echo
 
@@ -43,7 +43,7 @@ echo "Please enter your Vaultwarden domain."
 echo 'For example: "https://vaultwarden.domain.tld"'
 echo
 read -p 'Vaultwarden domain: ' domain_var
-sed "s@^# DOMAIN=.*@DOMAIN=$domain_var@" /etc/vaultwarden.env
+sed -i "s@^# DOMAIN=.*@DOMAIN=$domain_var@" /etc/vaultwarden.env
 echo
 
 echo "Generating self-signed SSL certificate..."
@@ -53,8 +53,8 @@ chown -R vaultwarden:vaultwarden /var/lib/vaultwarden/ssl
 echo
 
 echo "Enabling HTTPS..."
-sed 's@^# ROCKET_PORT=.*@ROCKET_PORT=443@' /etc/vaultwarden.env
-sed 's@^# ROCKET_TLS=.*@ROCKET_TLS={certs="/var/lib/vaultwarden/ssl/cert.pem",key="/var/lib/vaultwarden/ssl/key.pem"}@' /etc/vaultwarden.env
+sed -i 's@^# ROCKET_PORT=.*@ROCKET_PORT=443@' /etc/vaultwarden.env
+sed -i 's@^# ROCKET_TLS=.*@ROCKET_TLS={certs="/var/lib/vaultwarden/ssl/cert.pem",key="/var/lib/vaultwarden/ssl/key.pem"}@' /etc/vaultwarden.env
 echo
 
 echo "Configuring SMTP..."
@@ -63,16 +63,16 @@ echo
 read -p 'SMTP Server: ' smtp_server_var
 read -p 'Email Address: ' smtp_email_address_var
 read -p 'Password: ' smtp_password_var
-sed "s@^# SMTP_HOST=.*@SMTP_HOST=$smtp_server_var@" /etc/vaultwarden.env
-sed "s@^# SMTP_FROM=.*@SMTP_FROM=$smtp_email_address_var@" /etc/vaultwarden.env
-sed 's@^# SMTP_FROM_NAME=.*@SMTP_FROM_NAME=Vaultwarden@' /etc/vaultwarden.env
-sed 's@^# SMTP_PORT=.*@SMTP_PORT=587@' /etc/vaultwarden.env
-sed 's@^# SMTP_SSL=.*@SMTP_SSL=true@' /etc/vaultwarden.env
-sed 's@^# SMTP_EXPLICIT_TLS=.*@SMTP_EXPLICIT_TLS=false@' /etc/vaultwarden.env
-sed "s@^# SMTP_USERNAME=.*@SMTP_USERNAME=$smtp_email_address_var@" /etc/vaultwarden.env
-sed "s@^# SMTP_PASSWORD=.*@SMTP_PASSWORD=$smtp_password_var@" /etc/vaultwarden.env
-sed 's@^# SMTP_TIMEOUT=.*@SMTP_TIMEOUT=15@' /etc/vaultwarden.env
-sed 's@^# SMTP_AUTH_MECHANISM=.*@SMTP_AUTH_MECHANISM="Login"@' /etc/vaultwarden.env
+sed -i "s@^# SMTP_HOST=.*@SMTP_HOST=$smtp_server_var@" /etc/vaultwarden.env
+sed -i "s@^# SMTP_FROM=.*@SMTP_FROM=$smtp_email_address_var@" /etc/vaultwarden.env
+sed -i 's@^# SMTP_FROM_NAME=.*@SMTP_FROM_NAME=Vaultwarden@' /etc/vaultwarden.env
+sed -i 's@^# SMTP_PORT=.*@SMTP_PORT=587@' /etc/vaultwarden.env
+sed -i 's@^# SMTP_SSL=.*@SMTP_SSL=true@' /etc/vaultwarden.env
+sed -i 's@^# SMTP_EXPLICIT_TLS=.*@SMTP_EXPLICIT_TLS=false@' /etc/vaultwarden.env
+sed -i "s@^# SMTP_USERNAME=.*@SMTP_USERNAME=$smtp_email_address_var@" /etc/vaultwarden.env
+sed -i "s@^# SMTP_PASSWORD=.*@SMTP_PASSWORD=$smtp_password_var@" /etc/vaultwarden.env
+sed -i 's@^# SMTP_TIMEOUT=.*@SMTP_TIMEOUT=15@' /etc/vaultwarden.env
+sed -i 's@^# SMTP_AUTH_MECHANISM=.*@SMTP_AUTH_MECHANISM="Login"@' /etc/vaultwarden.env
 echo
 
 echo "Enabling and starting Vaultwarden..."
