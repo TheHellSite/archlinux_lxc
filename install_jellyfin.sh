b7ca71ceb409fbd03f846ff3cdb3939be9baacef
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2021-09-28 22:15:12 +0200

    Update install_jellyfin.sh

--- install_jellyfin.sh
+++ install_jellyfin.sh
@@ -58,9 +58,9 @@ sudo openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -ke
 sudo openssl pkcs12 -export -inkey /var/lib/jellyfin/ssl/key.pem -in /var/lib/jellyfin/ssl/cert.pem -out /var/lib/jellyfin/ssl/cert.pfx -passout pass:
 sudo rm /var/lib/jellyfin/ssl/*.pem
 sudo chown -R jellyfin:jellyfin /var/lib/jellyfin/ssl
-sed -i 's@^  <CertificatePath />@  <CertificatePath>/var/lib/jellyfin/ssl/cert.pfx</CertificatePath>@' /var/lib/jellyfin/config/network.xml
-sed -i 's@^  <EnableHttps>false</EnableHttps>@  <EnableHttps>true</EnableHttps>@' /var/lib/jellyfin/config/network.xml
-sed -i 's@^  <RequireHttps>false</RequireHttps>@  <RequireHttps>true</RequireHttps>@' /var/lib/jellyfin/config/network.xml
+sudo sed -i 's@^  <CertificatePath />@  <CertificatePath>/var/lib/jellyfin/ssl/cert.pfx</CertificatePath>@' /var/lib/jellyfin/config/network.xml
+sudo sed -i 's@^  <EnableHttps>false</EnableHttps>@  <EnableHttps>true</EnableHttps>@' /var/lib/jellyfin/config/network.xml
+sudo sed -i 's@^  <RequireHttps>false</RequireHttps>@  <RequireHttps>true</RequireHttps>@' /var/lib/jellyfin/config/network.xml
 echo
 echo "Starting Jellyfin..."
 sudo systemctl start jellyfin
