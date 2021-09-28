543fec19bf791422c69533feb94d9ed352a418bb
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2021-09-28 19:46:48 +0200

    Update install_jellyfin.sh

--- install_jellyfin.sh
+++ install_jellyfin.sh
@@ -26,7 +26,15 @@ echo "======================"
 read -p "Press ENTER to start..."
 echo
 mkdir git && cd git && git clone https://aur.archlinux.org/jellyfin-bin.git && cd jellyfin-bin && makepkg -sirc && cd && sudo rm -r git
-sudo systemctl enable --now jellyfin && sudo systemctl status jellyfin && sudo systemctl stop jellyfin
+echo
+echo "Enabling and starting Jellyfin to generate config files..."
+sudo systemctl enable --now jellyfin
+echo
+echo "Waiting 10 seconds for Jellyfin to start..."
+sleep 10
+echo
+echo "Stopping Jellyfin to edit config files..."
+sudo systemctl stop jellyfin
 echo
 echo
 echo
@@ -35,8 +43,17 @@ echo "Configuring Jellyfin..."
 echo "======================="
 read -p "Press ENTER to start..."
 echo
-#mkdir git && cd git && git clone https://aur.archlinux.org/jellyfin-bin.git && cd jellyfin-bin && makepkg -sirc && cd && sudo rm -r git
-#sudo systemctl enable --now jellyfin && sudo systemctl status jellyfin && sudo systemctl stop jellyfin
+echo "Enabling HTTPS..."
+sudo mkdir /var/lib/jellyfin/ssl
+sudo openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/jellyfin/ssl/key.pem -out /var/lib/jellyfin/ssl/cert.pem
+sudo openssl pkcs12 -export -inkey /var/lib/jellyfin/ssl/key.pem -in /var/lib/jellyfin/ssl/cert.pem -out /var/lib/jellyfin/ssl/jellyfin.pfx -passout pass:
+sudo chown jellyfin:jellyfin *
+echo
+echo "Starting Jellyfin..."
+sudo systemctl start jellyfin
+echo "Waiting 5 seconds for Jellyfin to start..."
+sleep 5
+sudo systemctl status jellyfin
 echo
 echo
 echo
