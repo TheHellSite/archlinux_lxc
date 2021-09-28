04f9c742737189adf269f26682bba75b4d23868e
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2021-09-28 21:55:25 +0200

    Update install_jellyfin.sh

--- install_jellyfin.sh
+++ install_jellyfin.sh
@@ -16,7 +16,7 @@ echo "Preparing AUR..."
 echo "================"
 read -p "Press ENTER to start..."
 echo
-sudo pacman -Syyu git base-devel --noconfirm
+sudo pacman -Syyu --noconfirm git base-devel
 echo
 echo
 echo
@@ -29,7 +29,7 @@ mkdir git
 cd git
 git clone https://aur.archlinux.org/jellyfin-bin.git
 cd jellyfin-bin
-makepkg -sirc
+makepkg -sirc --noconfirm
 cd
 sudo rm -r git
 echo
@@ -43,32 +43,24 @@ echo
 echo "Enabling and starting Jellyfin to generate config files..."
 sudo systemctl enable --now jellyfin
 echo
-
 echo "Waiting 10 seconds for Jellyfin to start..."
 sleep 10
 echo
-
 echo "Stopping Jellyfin to edit config files..."
 sudo systemctl stop jellyfin
 echo
-
 echo "Enabling HTTPS..."
 sudo mkdir /var/lib/jellyfin/ssl
 sudo openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/jellyfin/ssl/key.pem -out /var/lib/jellyfin/ssl/cert.pem
-sudo openssl pkcs12 -export -inkey /var/lib/jellyfin/ssl/key.pem -in /var/lib/jellyfin/ssl/cert.pem -out /var/lib/jellyfin/ssl/jellyfin.pfx -passout pass:
-sudo chown -R /var/lib/jellyfin/ssl
+sudo openssl pkcs12 -export -inkey /var/lib/jellyfin/ssl/key.pem -in /var/lib/jellyfin/ssl/cert.pem -out /var/lib/jellyfin/ssl/cert.pfx -passout pass:
+sudo rm /var/lib/jellyfin/config/ssl/*.pem
+sudo chown -R jellyfin:jellyfin /var/lib/jellyfin/ssl
+sed -i 's@^  <CertificatePath />@  <CertificatePath>/var/lib/jellyfin/ssl/cert.pfx</CertificatePath>@' /var/lib/jellyfin/config/network.xml
+sed -i 's@^  <EnableHttps>false</EnableHttps>@  <EnableHttps>true</EnableHttps>@' /var/lib/jellyfin/config/network.xml
+sed -i 's@^  <RequireHttps>false</RequireHttps>@  <RequireHttps>true</RequireHttps>@' /var/lib/jellyfin/config/network.xml
 echo
-
-CHANGE CONFIG
-
-
 echo "Starting Jellyfin..."
 sudo systemctl start jellyfin
 echo "Waiting 5 seconds for Jellyfin to start..."
 sleep 5
 sudo systemctl status jellyfin
-echo
-
-echo
-echo
-echo
