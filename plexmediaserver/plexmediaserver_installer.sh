d738a2dd822878e54ed8c089df18d58b8615c242
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-01-22 13:40:47 +0100

    Update plexmediaserver_installer.sh

--- plexmediaserver/plexmediaserver_installer.sh
+++ plexmediaserver/plexmediaserver_installer.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# begin of variables
+var_service_name="plexmediaserver"
+var_service_friendly_name="Plex Media Server"
+var_headline_length="================="
+var_local_ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+var_local_subnet=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | sed 's@[^.]*$@0/24@')
+# end of variables
+
 clear
-echo "================================================"
-echo "== Arch Linux LXC Plex Media Server Installer =="
-echo "================================================"
+echo "====================$var_headline_length============="
+echo "== Arch Linux LXC - $var_service_friendly_name Installer =="
+echo "====================$var_headline_length============="
 echo
-echo "This script will install Plex Media Server."
+echo "This script will install $var_service_friendly_name."
+
+echo "This script will install $var_service_name."
+
 echo
 read -p "Press ENTER to start the script."
 echo
@@ -23,8 +34,8 @@ echo
 echo
 echo
 
-echo "Installing Plex Media Server..."
-echo "==============================="
+echo "Installing $var_service_friendly_name..."
+echo "===========$var_headline_length==="
 read -p "Press ENTER to continue..."
 echo
 git clone https://aur.archlinux.org/plex-media-server.git
@@ -37,21 +48,19 @@ echo
 echo
 echo
 
-echo "Configuring Plex Media Server..."
-echo "================================"
+echo "Configuring $var_service_friendly_name..."
+echo "============$var_headline_length==="
 read -p "Press ENTER to continue..."
 echo
-echo "Enabling and starting Plex Media Server to generate config files..."
-sudo systemctl enable --now plexmediaserver
+echo "Enabling and starting $var_service_friendly_name to generate config files..."
+sudo systemctl enable --now $var_service_name
 echo
-echo "Waiting 10 seconds for Plex Media Server to start..."
+echo "Waiting 10 seconds for $var_service_friendly_name to start..."
 sleep 10
 echo
-echo "Stopping Plex Media Server to edit config files..."
-sudo systemctl stop plexmediaserver
+echo "Stopping $var_service_friendly_name to edit config files..."
+sudo systemctl stop $var_service_name
 echo
-var_local_ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
-var_local_subnet=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | sed 's@[^.]*$@0/24@')
 echo "Please enter a comma-separated list of IPs or subnets that are allowed without authentication."
 echo "You should at least specify the IP or subnet of the machine you are currently typing on in order to access the web interface after the installation."
 echo "For example: $var_local_ip,$var_local_subnet"
@@ -78,18 +87,18 @@ echo
 echo
 echo
 
-echo "Starting Plex Media Server..."
-echo "============================="
-echo "The installation and configuration of Plex Media Server is complete."
-echo "Proceed to start Plex Media Server."
+echo "Starting $var_service_friendly_name..."
+echo "=========$var_headline_length==="
+echo "The installation and configuration of $var_service_friendly_name is complete."
+echo "Proceed to start $var_service_friendly_name."
 echo
 read -p "Press ENTER to continue..."
 echo
-sudo systemctl start plexmediaserver
-echo "Waiting 5 seconds for Plex Media Server to start..."
+sudo systemctl start $var_service_name
+echo "Waiting 5 seconds for $var_service_friendly_name to start..."
 sleep 5
 echo
-echo "You can now access the Plex Media Server web interface to perform the Server Setup."
+echo "You can now access the $var_service_friendly_name web interface to perform the Server Setup."
 echo "http://$var_local_ip:32400/web/index.html"
 echo
 echo "After finishing the Server Setup you can also access the web interface using https."
@@ -99,4 +108,4 @@ echo "Proceed to display the service status and end the script."
 echo
 read -p "Press ENTER to continue..."
 echo
-sudo systemctl status plexmediaserver
+sudo systemctl status $var_service_name
