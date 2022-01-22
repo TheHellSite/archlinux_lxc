a393381ded1a1ff61b8773d032e7076385bc0908
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-01-22 15:48:21 +0100

    Update pyload_installer.sh

--- pyload/pyload_installer.sh
+++ pyload/pyload_installer.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# begin of variables
+var_service_name="pyload"
+var_service_friendly_name="pyLoad"
+var_service_friendly_name_length="======"
+var_service_default_port="8000"
+var_local_ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+var_local_subnet=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | sed 's@[^.]*$@0/24@')
+# end of variables
+
 clear
-echo "====================================="
-echo "== Arch Linux LXC pyLoad Installer =="
-echo "====================================="
+echo "====================$var_service_friendly_name_length============="
+echo "== Arch Linux LXC - $var_service_friendly_name Installer =="
+echo "====================$var_service_friendly_name_length============="
 echo
-echo "This script will install pyLoad."
+echo "This script will install $var_service_friendly_name."
 echo
 read -p "Press ENTER to start the script."
 echo
@@ -13,22 +22,23 @@ echo
 echo
 echo
 
-echo "Installing pyLoad..."
-echo "===================="
+echo "Installing $var_service_friendly_name..."
+echo "===========$var_service_friendly_name_length==="
 read -p "Press ENTER to continue..."
 echo
 echo "Installing dependencies..."
 pacman -Syyu --needed --noconfirm gcc python-pip
 echo
-echo "Installing pyLoad..."
+echo "Installing $var_service_friendly_name..."
 pip install --pre pyload-ng[all]
+pip install --upgrade https://github.com/pyload/pyload/archive/refs/heads/develop.zip
 echo
 echo
 echo
 echo
 
-echo "Configuring pyLoad..."
-echo "====================="
+echo "Configuring $var_service_friendly_name..."
+echo "============$var_service_friendly_name_length==="
 read -p "Press ENTER to continue..."
 echo
 echo "Creating user "pyload"..."
@@ -50,16 +60,16 @@ TimeoutSec=20
 WantedBy=multi-user.target
 EOF
 echo
-echo "Enabling and starting pyLoad to generate config files..."
+echo "Enabling and starting $var_service_friendly_name to generate config files..."
 mkdir -p /var/lib/pyload
 chown -R pyload:pyload /var/lib/pyload
-systemctl enable --now pyload
+systemctl enable --now $var_service_name
 echo
-echo "Waiting 10 seconds for pyLoad to start..."
+echo "Waiting 10 seconds for $var_service_friendly_name to start..."
 sleep 10
 echo
-echo "Stopping pyLoad to edit config files..."
-systemctl stop pyload
+echo "Stopping $var_service_friendly_name to edit config files..."
+systemctl stop $var_service_name
 echo
 echo "Generating self-signed SSL certificate..."
 mkdir -p /var/lib/pyload/ssl
@@ -76,19 +86,22 @@ echo
 echo
 echo
 
-echo "Starting pyLoad..."
-echo "=================="
-echo "The installation and configuration of pyLoad is complete."
-echo "Proceed to start pyLoad."
+echo "Starting $var_service_friendly_name..."
+echo "=========$var_service_friendly_name_length==="
+echo "The installation and configuration of $var_service_friendly_name is complete."
+echo "Proceed to start $var_service_friendly_name."
 echo
 read -p "Press ENTER to continue..."
 echo
-sudo systemctl start pyload
-echo "Waiting 5 seconds for pyLoad to start..."
+sudo systemctl start $var_service_name
+echo "Waiting 5 seconds for $var_service_friendly_name to start..."
 sleep 5
+echo
+echo "You can now access the $var_service_friendly_name web interface to perform the Server Setup."
+echo "http://$var_local_ip:$var_service_default_port"
 echo
 echo "Proceed to display the service status and end the script."
 echo
 read -p "Press ENTER to continue..."
 echo
-sudo systemctl status pyload
+sudo systemctl status $var_service_name
