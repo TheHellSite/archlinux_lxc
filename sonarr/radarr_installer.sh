6137e1b8b2ad3c5b8ab4c2c1f14c158da5882930
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-02-23 22:35:36 +0100

    Create radarr_installer.sh

---
+++ sonarr/radarr_installer.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# begin of variables
+var_service_name="sonarr"
+var_service_friendly_name="Sonarr"
+var_service_friendly_name_length="======"
+var_service_default_port="8989"
+var_local_ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+var_local_subnet=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | sed 's@[^.]*$@0/24@')
+# end of variables
+
+clear
+echo "====================$var_service_friendly_name_length============="
+echo "== Arch Linux LXC - $var_service_friendly_name Installer =="
+echo "====================$var_service_friendly_name_length============="
+echo
+echo "This script will install $var_service_friendly_name."
+echo
+read -p "Press ENTER to start the script."
+echo
+echo
+echo
+echo
+
+echo "Preparing AUR..."
+echo "================"
+read -p "Press ENTER to continue..."
+echo
+sudo pacman -Syyu --needed --noconfirm git base-devel
+echo
+echo
+echo
+echo
+
+echo "Installing $var_service_friendly_name..."
+echo "===========$var_service_friendly_name_length==="
+read -p "Press ENTER to continue..."
+echo
+git clone https://aur.archlinux.org/sonarr.git
+cd sonarr
+makepkg -sirc --noconfirm
+cd
+sudo rm -r sonarr
+echo
+echo
+echo
+echo
+
+echo "Configuring $var_service_friendly_name..."
+echo "============$var_service_friendly_name_length==="
+read -p "Press ENTER to continue..."
+echo
+echo "Enabling and starting $var_service_friendly_name to generate config files..."
+sudo systemctl enable --now $var_service_name &> /dev/null
+echo
+echo "Waiting 10 seconds for $var_service_friendly_name to start..."
+sleep 10
+echo
+echo "Stopping $var_service_friendly_name to edit config files..."
+sudo systemctl stop $var_service_name
+echo
+# asd
+echo
+echo
+echo
+echo
+
+echo "Starting $var_service_friendly_name..."
+echo "=========$var_service_friendly_name_length==="
+echo "The installation and configuration of $var_service_friendly_name is complete."
+echo "Proceed to start $var_service_friendly_name."
+echo
+read -p "Press ENTER to continue..."
+echo
+sudo systemctl start $var_service_name
+echo "Waiting 5 seconds for $var_service_friendly_name to start..."
+sleep 5
+echo
+echo "You can now access the $var_service_friendly_name web interface to perform the Server Setup."
+echo "http://$var_local_ip:$var_service_default_port/web/index.html"
+echo
+echo "After finishing the Server Setup you can also access the web interface using https."
+echo "https://$var_local_ip:$var_service_default_port/web/index.html"
+echo
+echo "Proceed to display the service status and end the script."
+echo
+read -p "Press ENTER to continue..."
+echo
+sudo systemctl status $var_service_name
