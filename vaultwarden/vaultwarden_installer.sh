38cc15fdd1906b3a1b96204237b013bcc5cad07a
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-03-19 23:55:46 +0100

    Update vaultwarden_installer.sh

--- vaultwarden/vaultwarden_installer.sh
+++ vaultwarden/vaultwarden_installer.sh
@@ -55,7 +55,7 @@ echo "Generating self-signed SSL certificate..."
 mkdir -p /var/lib/vaultwarden/ssl
 openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/vaultwarden/ssl/key.pem -out /var/lib/vaultwarden/ssl/cert.pem &> /dev/null
 chown -R vaultwarden:vaultwarden /var/lib/vaultwarden/ssl
-chmod 0750 /var/lib/vaultwarden/ssl
+chmod 0755 /var/lib/vaultwarden/ssl
 chmod 0640 /var/lib/vaultwarden/ssl/*
 echo
 echo "Configuring Rocket web framework..."
