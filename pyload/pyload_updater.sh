d028dff3f1ce7169fa4486fcf52deff2ec45a080
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-10-20 15:44:36 +0200

    Create pyload_updater.sh

---
+++ pyload/pyload_updater.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# begin of variables
+var_service_name="pyload"
+var_service_friendly_name="pyLoad"
+var_service_friendly_name_length="======"
+# end of variables
+
+clear
+echo "====================$var_service_friendly_name_length==========="
+echo "== Arch Linux LXC - $var_service_friendly_name Updater =="
+echo "====================$var_service_friendly_name_length==========="
+echo
+echo "This script will update $var_service_friendly_name."
+echo
+read -p "Press ENTER to start the script."
+echo
+echo
+echo
+echo
+
+echo "Updating $var_service_friendly_name..."
+echo "=========$var_service_friendly_name_length==="
+read -p "Press ENTER to continue..."
+echo
+echo "Stopping $var_service_friendly_name..."
+sudo systemctl stop $var_service_name
+echo
+echo "Updating $var_service_friendly_name..."
+pip install --upgrade --pre pyload-ng[plugins]
+echo
+echo
+echo
+echo
+
+echo "Restarting $var_service_friendly_name..."
+echo "===========$var_service_friendly_name_length==="
+echo "The $var_service_friendly_name update is complete."
+echo "Proceed to start $var_service_friendly_name and display the service status."
+echo
+read -p "Press ENTER to continue..."
+echo
+sudo systemctl start $var_service_name
+echo "Waiting 5 seconds for $var_service_friendly_name to start..."
+sleep 5
+echo
+sudo systemctl status $var_service_name
