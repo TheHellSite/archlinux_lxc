1df405ca05267f3b349c0098a36f035d6c05d137
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-11-13 01:05:29 +0100

    Update jdownloader_installer.sh

--- jdownloader/jdownloader_installer.sh
+++ jdownloader/jdownloader_installer.sh
@@ -30,7 +30,7 @@ pacman -Syyu --needed --noconfirm jre-openjdk-headless
 echo
 echo "Installing $var_service_friendly_name..."
 mkdir -p /var/lib/jdownloader
-curl https://installer.jdownloader.org/JDownloader.jar -o JDownloader.jar
+curl https://installer.jdownloader.org/JDownloader.jar -o /var/lib/jdownloader/JDownloader.jar
 echo
 echo
 echo
@@ -61,42 +61,13 @@ TimeoutSec=20
 WantedBy=multi-user.target
 EOF
 echo
-echo "Enabling and starting $var_service_friendly_name to generate config files..."
-mkdir -p /var/lib/pyload
-chown -R pyload:pyload /var/lib/pyload
-systemctl enable --now $var_service_name &> /dev/null
+echo "Enabling service $var_service_friendly_name..."
+chown -R jdownloader:jdownloader /var/lib/jdownloader
+systemctl enable $var_service_name &> /dev/null
 echo
-echo "Waiting 10 seconds for $var_service_friendly_name to start..."
-sleep 10
-echo
-echo "Stopping $var_service_friendly_name to edit config files..."
-systemctl stop $var_service_name
-echo
-echo "Generating self-signed SSL certificate..."
-mkdir -p /var/lib/pyload/ssl
-openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/pyload/ssl/key.pem -out /var/lib/pyload/ssl/cert.pem &> /dev/null
-chown -R pyload:pyload /var/lib/pyload/ssl
-chmod 0755 /var/lib/pyload/ssl
-chmod 0640 /var/lib/pyload/ssl/*
-echo
-echo "Configuring Download..."
-mkdir -p /tmp/pyload
-sed -i 's@int chunks : "Maximum connections for one download" =.*@int chunks : "Maximum connections for one download" = 4@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@ip interface : "Download interface to bind (IP Address)" =.*@ip interface : "Download interface to bind (IP Address)" = 0.0.0.0@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@int max_downloads : "Maximum parallel downloads" =.*@int max_downloads : "Maximum parallel downloads" = 4@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@bool skip_existing : "Skip already existing files" =.*@bool skip_existing : "Skip already existing files" = True@' /var/lib/pyload/settings/pyload.cfg
-echo
-echo "Configuring General..."
-sed -i 's@bool debug_mode : "Debug mode" =.*@bool debug_mode : "Debug mode" = False@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@folder storage_folder : "Download folder" =.*@folder storage_folder : "Download folder" = /tmp/pyload@' /var/lib/pyload/settings/pyload.cfg
-echo
-echo "Configuring Web Interface..."
-sed -i 's@bool develop : "Development mode" =.*@bool develop : "Development mode" = False@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@ip host : "IP address" =.*@ip host : "IP address" = 0.0.0.0@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@file ssl_certchain : "CA'\''s intermediate certificate bundle (optional)" =.*@file ssl_certchain : "CA'\''s intermediate certificate bundle (optional)" =@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@file ssl_certfile : "SSL Certificate" =.*@file ssl_certfile : "SSL Certificate" = /var/lib/pyload/ssl/cert.pem@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@file ssl_keyfile : "SSL Key" =.*@file ssl_keyfile : "SSL Key" = /var/lib/pyload/ssl/key.pem@' /var/lib/pyload/settings/pyload.cfg
-sed -i 's@bool use_ssl : "Use HTTPS" =.*@bool use_ssl : "Use HTTPS" = True@' /var/lib/pyload/settings/pyload.cfg
+echo "Starting JDownloader to install available updates..."
+sleep 5
+/usr/bin/java -Djava.awt.headless=true -jar --userdir /var/lib/jdownloader/JDownloader.jar
 echo
 echo
 echo
@@ -104,20 +75,10 @@ echo
 
 echo "Starting $var_service_friendly_name..."
 echo "=========$var_service_friendly_name_length==="
-echo "The installation and configuration of $var_service_friendly_name is complete."
-echo "Proceed to start $var_service_friendly_name."
+echo "The installation and configuration of $var_service_friendly_name is almost complete."
+echo "Proceed to start $var_service_friendly_name and enter your MyJDownloader credentials."
+echo "After connecting your instance to MyJdownloader press CTRL+C and reboot the LXC."
 echo
 read -p "Press ENTER to continue..."
 echo
-systemctl start $var_service_name
-echo "Waiting 5 seconds for $var_service_friendly_name to start..."
-sleep 5
-echo
-echo "You can now access the $var_service_friendly_name web interface."
-echo "https://$var_local_ip:$var_service_default_port"
-echo
-echo "Proceed to display the service status and end the script."
-echo
-read -p "Press ENTER to continue..."
-echo
-systemctl status $var_service_name
+/usr/bin/java -Djava.awt.headless=true -jar --userdir /var/lib/jdownloader/JDownloader.jar
