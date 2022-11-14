6948d32007a066b649b9e93b717b93a49f9d218d
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2022-11-14 17:07:00 +0100

    Update jdownloader_installer.sh

--- jdownloader/jdownloader_installer.sh
+++ jdownloader/jdownloader_installer.sh
@@ -26,7 +26,7 @@ echo "===========$var_service_friendly_name_length==="
 read -p "Press ENTER to continue..."
 echo
 echo "Installing dependencies..."
-pacman -Syyu --needed --noconfirm jre-openjdk-headless
+pacman -Syyu --needed --noconfirm jre-openjdk-headless ffmpeg
 echo
 echo "Installing $var_service_friendly_name..."
 mkdir -p /var/lib/jdownloader
