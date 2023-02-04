52f6ba7e812ddbb5fb2fce74b4b196411a69961f
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2023-02-04 01:26:14 +0100

    Create bazarr_update.sh

---
+++ bazarr/bazarr_update.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# begin of variables
+var_service_name="bazarr"
+var_service_friendly_name="Bazarr"
+var_service_friendly_name_length="======"
+# end of variables
+
+clear
+echo "====================$var_service_friendly_name_length==========="
+echo "== Arch Linux LXC - $var_service_friendly_name Updater =="
+echo "====================$var_service_friendly_name_length==========="
+echo
+echo "This script will update $var_service_friendly_name."
+echo
+read -p "Press ENTER to start the script."
+echo
+echo
+echo
+echo
+
+echo "Updating $var_service_friendly_name..."
+echo "=========$var_service_friendly_name_length==="
+read -p "Press ENTER to continue..."
+echo
+echo "Stopping $var_service_friendly_name..."
+sudo systemctl stop $var_service_name
+echo
+echo "Updating dependencies..."
+git clone https://aur.archlinux.org/python-webrtcvad-wheels.git
+cd python-webrtcvad-wheels
+makepkg -sirc --noconfirm
+cd
+sudo rm -r python-webrtcvad-wheels
+echo
+echo "Updating $var_service_friendly_name..."
+git clone https://aur.archlinux.org/bazarr.git
+cd bazarr
+makepkg -sirc --noconfirm
+cd
+sudo rm -r bazarr
+echo
+echo
+echo
+echo
+
+echo "Restarting $var_service_friendly_name..."
+echo "===========$var_service_friendly_name_length==="
+echo "The $var_service_friendly_name update is complete."
+echo "Proceed to start $var_service_friendly_name and display the service status."
+echo
+read -p "Press ENTER to continue..."
+echo
+sudo systemctl start $var_service_name
+echo "Waiting 5 seconds for $var_service_friendly_name to start..."
+sleep 5
+echo
+sudo systemctl status $var_service_name
