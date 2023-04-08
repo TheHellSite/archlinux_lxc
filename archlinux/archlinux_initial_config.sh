80bc4948a08f73dbaaf0236440cf121eefcfdf14
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2023-04-08 12:58:04 +0200

    Update archlinux_initial_config.sh

--- archlinux/archlinux_initial_config.sh
+++ archlinux/archlinux_initial_config.sh
@@ -48,7 +48,7 @@ done
 #echo 'LC_ALL=en_US.UTF-8' >| /etc/environment
 #echo 'en_US.UTF-8 UTF-8' >| /etc/locale.gen
 #echo 'LANG=en_US.UTF-8' >| /etc/locale.conf
-locale-gen
+#locale-gen
 echo
 echo
 echo
