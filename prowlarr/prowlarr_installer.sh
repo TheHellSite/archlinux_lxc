c546c4daaf3ebf096ca67c0450e7d87eb5be8d78
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2023-04-19 11:44:53 +0200

    Create prowlarr_installer.sh

---
+++ prowlarr/prowlarr_installer.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# begin of variables
+var_service_name="prowlarr"
+var_service_friendly_name="Prowlarr"
+var_service_friendly_name_length="======"
+var_service_default_port="9898"
+var_local_ip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+var_local_subnet=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}' | sed 's@[^.]*$@0/24@')
+# end of variables
+
+clear
+echo "====================$var_service_friendly_name_length============="
+echo "== Arch Linux LXC - $var_service_friendly_name Installer =="
+echo "====================$var_service_friendly_name_length============="
+echo
+echo "This script will install $var_service_friendly_name."
+echo
+read -p "Press ENTER to start the script."
+echo
+echo
+echo
+echo
+
+echo "Preparing AUR..."
+echo "================"
+read -p "Press ENTER to continue..."
+echo
+sudo pacman -Syyu --needed --noconfirm git base-devel
+echo
+echo
+echo
+echo
+
+echo "Installing $var_service_friendly_name..."
+echo "===========$var_service_friendly_name_length==="
+read -p "Press ENTER to continue..."
+echo
+git clone https://aur.archlinux.org/prowlarr.git
+cd prowlarr
+makepkg -sirc --noconfirm
+cd
+sudo rm -r prowlarr
+echo
+echo
+echo
+echo
+
+echo "Configuring $var_service_friendly_name..."
+echo "============$var_service_friendly_name_length==="
+read -p "Press ENTER to continue..."
+echo
+echo "Enabling and starting $var_service_friendly_name to generate config files..."
+sudo systemctl enable --now $var_service_name &> /dev/null
+echo
+echo "Waiting 10 seconds for $var_service_friendly_name to start..."
+sleep 10
+echo
+echo "Stopping $var_service_friendly_name to edit config files..."
+sudo systemctl stop $var_service_name
+echo
+echo "Generating self-signed SSL certificate..."
+sudo mkdir -p /var/lib/prowlarr/ssl
+sudo openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/prowlarr/ssl/key.pem -out /var/lib/prowlarr/ssl/cert.pem &> /dev/null
+sudo openssl rsa -in /var/lib/prowlarr/ssl/key.pem -outform pvk -pvk-none -out /var/lib/prowlarr/ssl/key.pvk &> /dev/null
+sudo openssl x509 -inform pem -in /var/lib/prowlarr/ssl/cert.pem -outform der -out /var/lib/prowlarr/ssl/cert.crt &> /dev/null
+sudo rm /var/lib/prowlarr/ssl/*.pem
+sudo chown -R prowlarr:prowlarr /var/lib/prowlarr/ssl
+sudo chmod 0755 /var/lib/prowlarr/ssl
+sudo chmod 0640 /var/lib/prowlarr/ssl/*
+echo
+echo "Enabling HTTPS..."
+sudo su -s /bin/bash -c "httpcfg -add -port 9898 -pvk /var/lib/prowlarr/ssl/key.pvk -cert /var/lib/prowlarr/ssl/cert.crt" prowlarr
+sudo sed -i 's@<EnableSsl>False</EnableSsl>@<EnableSsl>True</EnableSsl>@' /var/lib/prowlarr/config.xml
+echo
+echo "Disabling Analytics..."
+sudo sh -c "if grep -q '<AnalyticsEnabled>True</AnalyticsEnabled>' /var/lib/prowlarr/config.xml; then
+  sed -i 's/<AnalyticsEnabled>True<\/AnalyticsEnabled>/<AnalyticsEnabled>False<\/AnalyticsEnabled>/' /var/lib/prowlarr/config.xml
+fi"
+echo
+echo
+echo
+echo
+
+echo "Starting $var_service_friendly_name..."
+echo "=========$var_service_friendly_name_length==="
+echo "The installation and configuration of $var_service_friendly_name is complete."
+echo "Proceed to start $var_service_friendly_name."
+echo
+read -p "Press ENTER to continue..."
+echo
+sudo systemctl start $var_service_name
+echo "Waiting 5 seconds for $var_service_friendly_name to start..."
+sleep 5
+echo
+echo "You can now access the $var_service_friendly_name web interface."
+echo "https://$var_local_ip:$var_service_default_port"
+echo
+echo "Proceed to display the service status and end the script."
+echo
+read -p "Press ENTER to continue..."
+echo
+sudo systemctl status $var_service_name
