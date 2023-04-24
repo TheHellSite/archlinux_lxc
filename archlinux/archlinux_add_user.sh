e7d3e9a344d0ad45c233ec3db47b7ddd1c8b4acf
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2023-04-24 22:28:05 +0200

    Update archlinux_add_user.sh

--- archlinux/archlinux_add_user.sh
+++ archlinux/archlinux_add_user.sh
@@ -5,9 +5,7 @@ echo "============================="
 echo "== Arch Linux LXC add user =="
 echo "============================="
 echo
-echo "This script adds a new user"
-echo "to your Arch Linux LXC and"
-echo "adds it to group wheel."
+echo "This script creates a new non-root user in your Arch Linux LXC."
 echo
 read -p "Press ENTER to start the script."
 echo
@@ -15,21 +13,57 @@ echo
 echo
 echo
 
-echo "Adding new user"
-echo "==============="
-read -p "Press ENTER to continue..."
+echo "Create non-root user"
+echo "===================="
+echo 'You can now create a non-root user that is a member of group "wheel", thereby granting them "sudo" privileges.'
+echo "This is essential if you intend to install packages from the Arch User Repository (AUR)."
 echo
-echo 'Installing "sudo"...'
-pacman -Syyu --needed --noconfirm sudo
-echo 'Allowing members of group "wheel" to use "sudo"...'
-sed -i 's@^# %wheel ALL=(ALL:ALL) ALL@%wheel ALL=(ALL:ALL) ALL@' /etc/sudoers
-sed -i 's@^# %wheel ALL=(ALL) ALL@%wheel ALL=(ALL) ALL@' /etc/sudoers
-echo
-echo 'Creating new user that is a member of group "wheel"...'
-read -p 'Username: ' username_var
-read -s -p 'Password: ' password_var
-useradd -m -g users -G wheel -s /bin/bash $username_var
-echo "$username_var:$password_var" | chpasswd
+while true; do
+  read -p "Would you like to create a non-root user? [no]: " -r
+  # Set default value for REPLY to "no"
+  REPLY=${REPLY:-"no"}
+  if [[ "${REPLY,,}" == "y" || "${REPLY,,}" == "yes" ]]; then
+    echo
+    var_username_default="aur"
+    read -p "Enter username [$var_username_default]: " var_username_input
+    var_username=${var_username_input:-$var_username_default}
+    while ! [[ "$var_username" =~ ^[a-zA-Z0-9_]+$ ]]; do
+      echo "Invalid username. Only upper-/lowercase letters, numbers, and underscores are allowed."
+      echo
+      read -p "Enter username [$var_username_default]: " var_username_input
+      var_username=${var_username_input:-$var_username_default}
+    done
+    while true; do
+      read -s -p "Enter password: " var_password
+      echo
+      read -s -p "Retype password: " var_password_retype
+      echo
+      if [[ "$var_password" == "$var_password_retype" ]]; then
+        break
+      else
+        echo "Passwords do not match. Please try again."
+      fi
+    done
+    echo
+    echo "Creating non-root user \"$var_username\" as a member of group \"wheel\"..."
+    useradd -m -g users -G wheel -s /bin/bash "$var_username"
+    echo "$var_username:$var_password" | chpasswd
+    echo
+    echo 'Installing "sudo"...'
+    pacman -S --needed --noconfirm sudo
+    echo 'Allowing members of group "wheel" to use "sudo"...'
+    sed -i 's@^# %wheel ALL=(ALL:ALL) ALL@%wheel ALL=(ALL:ALL) ALL@' /etc/sudoers
+    sed -i 's@^# %wheel ALL=(ALL) ALL@%wheel ALL=(ALL) ALL@' /etc/sudoers
+    break
+  elif [[ "${REPLY,,}" == "n" || "${REPLY,,}" == "no" ]]; then
+    echo
+    echo "A non-root user will not be created."
+    break
+  else
+    echo 'Invalid input. Please enter "yes" or "no".'
+    echo
+  fi
+done
 echo
 echo
 echo
