c809bb575d8c31bfcaf186d1d612a30384ed3938
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2023-04-24 15:08:30 +0200

    Update jellyfin_installer.sh

--- jellyfin/jellyfin_installer.sh
+++ jellyfin/jellyfin_installer.sh
@@ -57,11 +57,11 @@ echo "============$var_service_friendly_name_length==="
 read -p "Press ENTER to continue..."
 echo
 echo 'Enabling auto-restart after crashes...'
-sed -i 's@^Restart=.*@Restart=on-failure@' /usr/lib/systemd/system/jellyfin.service
+sudo sed -i 's@^Restart=.*@Restart=on-failure@' /usr/lib/systemd/system/jellyfin.service
 if grep -q '^RestartSec=' /usr/lib/systemd/system/jellyfin.service; then
-  sed -i 's@^RestartSec=.*@RestartSec=5s@' /usr/lib/systemd/system/jellyfin.service
+  sudo sed -i 's@^RestartSec=.*@RestartSec=5s@' /usr/lib/systemd/system/jellyfin.service
 else
-  sed -i '/^Restart=on-failure/a RestartSec=5s' /usr/lib/systemd/system/jellyfin.service
+  sudo sed -i '/^Restart=on-failure/a RestartSec=5s' /usr/lib/systemd/system/jellyfin.service
 fi
 sudo systemctl daemon-reload
 echo
