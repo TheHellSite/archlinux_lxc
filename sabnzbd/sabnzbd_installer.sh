25eda3381a1840d65c9f42e3701096739e154eaf
TheHellSite <34900685+TheHellSite@users.noreply.github.com>
2024-10-17 21:41:49 +0200

    Update sabnzbd_installer.sh

--- sabnzbd/sabnzbd_installer.sh
+++ sabnzbd/sabnzbd_installer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # begin of variables
-var_service_name="sabnzbd"
-var_service_friendly_name="SABnzbd"
-var_service_friendly_name_length="======="
-var_service_default_port="8080"
 var_local_ip=$(ip route get 1.1.1.1 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+var_service_default_port="8080"
+var_service_dependencies="p7zip par2cmdline python python-pip python-setuptools unrar"
+var_service_download_url=$(curl -s https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest | grep 'browser_download_url' | grep 'src.tar.gz' | cut -d\" -f4)
+var_service_friendly_name="SABnzbd"
+var_service_friendly_name_length=${var_service_friendly_name//?/=}
+var_service_name=${var_service_friendly_name,,}
 # end of variables
 
 clear
@@ -26,16 +28,19 @@ echo "===========$var_service_friendly_name_length==="
 read -p "Press ENTER to continue..."
 echo
 echo "Installing dependencies..."
-pacman -Syu --needed --noconfirm base-devel ffmpeg git par2cmdline p7zip python python-pip python-wheel unrar unzip
+pacman -Syu --needed --noconfirm ${var_service_dependencies}
 echo
 echo "Installing $var_service_friendly_name..."
-git clone https://github.com/sabnzbd/sabnzbd.git /var/lib/${var_service_name}/git
-# Create python venv
-python3 -m venv /var/lib/${var_service_name}/venv
-source /var/lib/${var_service_name}/venv/bin/activate
-# Install requirements into the venv
-pip install --cache-dir /tmp/pip-cache -r /var/lib/${var_service_name}/git/requirements.txt
+mkdir -p /tmp/${var_service_name}_installer/${var_service_name}
+curl -L -o /tmp/${var_service_name}_installer/${var_service_name}_latest_linux.tar.gz "${var_service_download_url}"
+tar -xzf /tmp/${var_service_name}_installer/${var_service_name}_latest_linux.tar.gz --strip-components=1 -C /tmp/${var_service_name}_installer/${var_service_name}
+mv /tmp/${var_service_name}_installer/${var_service_name} /opt/${var_service_name}
+python3 -m venv /opt/${var_service_name}/python_venv
+source /opt/${var_service_name}/python_venv/bin/activate
+python3 -m pip install --upgrade pip wheel
+pip install --cache-dir /tmp/${var_service_name}_installer/pip-cache -r /opt/${var_service_name}/requirements.txt
 deactivate
+rm -r /tmp/${var_service_name}_installer
 echo
 echo
 echo
@@ -58,7 +63,7 @@ After=network.target
 Type=simple
 User=${var_service_name}
 Group=${var_service_name}
-ExecStart=/var/lib/${var_service_name}/venv/bin/python3 /var/lib/${var_service_name}/git/SABnzbd.py -s 0.0.0.0:8080 -f /var/lib/${var_service_name}/config/sabnzbd.ini
+ExecStart=/opt/${var_service_name}/python_venv/bin/python3 /opt/${var_service_name}/SABnzbd.py -s 0.0.0.0:8080 -f /var/lib/${var_service_name}/sabnzbd.ini
 Restart=always
 RestartSec=5s
 TimeoutSec=20
@@ -68,8 +73,8 @@ WantedBy=multi-user.target
 EOF
 echo
 echo "Enabling and starting ${var_service_friendly_name} to generate config files..."
-mkdir -p /var/lib/${var_service_name}/config
-chown -R ${var_service_name}:${var_service_name} /var/lib/${var_service_name}
+mkdir -p /var/lib/${var_service_name}
+chown -R ${var_service_name}:${var_service_name} /opt/${var_service_name} /var/lib/${var_service_name}
 systemctl enable --now ${var_service_name} &> /dev/null
 echo
 echo "Waiting 10 seconds for ${var_service_friendly_name} to start..."
@@ -79,16 +84,16 @@ echo "Stopping ${var_service_friendly_name} to edit config files..."
 systemctl stop ${var_service_name}
 echo
 echo "Generating self-signed SSL certificate..."
-mkdir -p /var/lib/${var_service_name}/config/ssl
-openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/${var_service_name}/config/ssl/server.key -out /var/lib/${var_service_name}/config/ssl/server.cert &> /dev/null
-chown -R ${var_service_name}:${var_service_name} /var/lib/${var_service_name}/config/ssl
-chmod 0755 /var/lib/${var_service_name}/config/ssl
-chmod 0640 /var/lib/${var_service_name}/config/ssl/*
+mkdir -p /var/lib/${var_service_name}/ssl
+openssl req -x509 -newkey rsa:4096 -sha512 -days 36500 -nodes -subj "/" -keyout /var/lib/${var_service_name}/ssl/server.key -out /var/lib/${var_service_name}/ssl/server.cert &> /dev/null
+chown -R ${var_service_name}:${var_service_name} /var/lib/${var_service_name}/ssl
+chmod 0755 /var/lib/${var_service_name}/ssl
+chmod 0640 /var/lib/${var_service_name}/ssl/*
 echo
 echo "Enabling HTTPS..."
-sed -i "s@https_cert = server.cert@https_cert = /var/lib/${var_service_name}/config/ssl/server.cert@" /var/lib/${var_service_name}/config/sabnzbd.ini
-sed -i "s@https_key = server.key@https_key = /var/lib/${var_service_name}/config/ssl/server.key@" /var/lib/${var_service_name}/config/sabnzbd.ini
-sed -i 's@enable_https = 0@enable_https = 1@' /var/lib/${var_service_name}/config/sabnzbd.ini
+sed -i "s@https_cert = server.cert@https_cert = /var/lib/${var_service_name}/ssl/server.cert@" /var/lib/${var_service_name}/sabnzbd.ini
+sed -i "s@https_key = server.key@https_key = /var/lib/${var_service_name}/ssl/server.key@" /var/lib/${var_service_name}/sabnzbd.ini
+sed -i 's@enable_https = 0@enable_https = 1@' /var/lib/${var_service_name}/sabnzbd.ini
 echo
 echo
 echo
